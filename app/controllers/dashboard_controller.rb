4d5192fcc7d1676bc6ee720cce70354e582a7ee3
Derek <wassabieboi@gmail.com>
2013-10-10 12:05:50 -0700

    Added models and controllers

---
+++ app/controllers/dashboard_controller.rb
@@ -0,0 +1,7 @@
+class DashboardController < ApplicationController
+
+	def dashboard
+		puts 'hello world'
+	end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Lunchathon::Application.routes.draw do
 
   # Sample resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+  match '/dashboard' => 'dashboard#dashboard', :as => 'dashboard'
 
   # Sample resource route with options:
   #   resources :products do
