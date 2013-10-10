4d774441e564605470e7f67b4852c54c842c892c
saasbook <saasbook@saasbook.(none)>
2013-10-10 10:57:35 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lunchathon::Application.config.secret_token = 'c7b272212177d52d71ed0e877f50125d64146ab7cd7e9beed18823dd4b66292d3002ade974b8f89473d39836b453bf7577f25b66c976fa2b86e02d1fac73cb8f'
