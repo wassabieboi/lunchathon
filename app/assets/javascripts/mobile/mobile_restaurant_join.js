eec425d5b0c0fd53f2426da4426ddf2452d2d5c2
Derek <wassabieboi@gmail.com>
2013-10-13 21:48:14 -0700

    Added logout, trying to fix weird two row navbar

--- app/assets/javascripts/mobile/mobile_restaurant_join.js
+++ app/assets/javascripts/mobile/mobile_restaurant_join.js
@@ -1,12 +1,14 @@
 function getRestaurantUsers() {
-  var restaurantId = $('#restaurant-id').text()
-  $.getJSON('/get_users/', {'restaurant_id' : restaurantId}, function(restaurantUsers) {
-    $('#panel-restaurant-name').text(restaurantUsers.restaurant_name);
-    var users = restaurantUsers.users;
-    for (var i = users.length - 1; i >= 0; i--) {
-      var currentUser = users[i];
-      $('#restaurant-user-table > tbody').append('<tr><td>' + currentUser.displayname + '</td></tr>');
-    };
+  $(function() {
+    var restaurantId = $('#restaurant-id').text()
+    $.getJSON('/get_users/', {'restaurant_id' : restaurantId}, function(restaurantUsers) {
+      $('#panel-restaurant-name').text(restaurantUsers.restaurant_name);
+      var users = restaurantUsers.users;
+      for (var i = users.length - 1; i >= 0; i--) {
+        var currentUser = users[i];
+        $('#restaurant-user-table > tbody').append('<tr><td>' + currentUser.displayname + '</td></tr>');
+      };
+    });
   });
 }
 
