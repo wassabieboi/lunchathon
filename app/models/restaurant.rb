155b15be023165692665f8aba57674044e3c9f91
Derek <wassabieboi@gmail.com>
2013-11-21 02:14:08 -0800

    More PostgreSQL shenanigans

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -3,16 +3,16 @@ class Restaurant < ActiveRecord::Base
   attr_accessible :name, :is_active
 
   scope :top5,
-    select("restaurants.id, restaurants.name, users.username, users.displayname, count(users.id) as user_count").
+    select("restaurants.id, restaurants.name, max(users.username), max(users.displayname), count(users.id) as user_count").
     joins("LEFT JOIN users on users.restaurant_id = restaurants.id").
-    group("restaurants.id, users.id").
+    group("restaurants.id").
     order("user_count DESC").
     limit(5)
 
   scope :all_by_desc_users,
-    select("restaurants.id, restaurants.name, users.username, users.displayname, count(users.id) as user_count").
+    select("restaurants.id, restaurants.name, max(users.username), max(users.displayname), count(users.id) as user_count").
     joins("LEFT JOIN users on users.restaurant_id = restaurants.id").
-    group("restaurants.id, users.id").
+    group("restaurants.id").
     order("user_count DESC")
 
   validates :name, :uniqueness => true, :presence => true
