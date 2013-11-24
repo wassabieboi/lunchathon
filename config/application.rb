703132b79c334b414e33097db522bac3a869a3db
Derek <wassabieboi@gmail.com>
2013-11-24 01:16:33 -0800

    Added new username label and hopefully fixed random heroku internal server errors by adding multithreaded config line

--- config/application.rb
+++ config/application.rb
@@ -58,5 +58,8 @@ module Lunchathon
 
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
+
+    # enable threaded mode
+    config.threadsafe!
   end
 end
